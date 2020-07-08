d888921a00a9cbdff6dfd47e34a743a84c1b4aa0
Damian Choptiany <damian.choptiany@gmail.com>
2020-07-08 10:07:12 +0200

    General List.hpp refactor

--- Include/List.hpp
+++ Include/List.hpp
@@ -7,195 +7,220 @@ class List
 {
 public:
     std::shared_ptr<Node<T>> first;
+    List();
+    List(const std::initializer_list<T>&);
+    void push_front(T);
+    void push_back(T);
+    void pop_front();
+    void pop_back();
+    void clear();
+    void print();
+    bool empty();
+    size_t size();
+    T at(size_t);
+    T min();
+    T max();
+};
 
-    List()
+template<class T>
+List<T>::List()
+{
+    first = nullptr;
+}
+
+template<class T>
+List<T>::List(const std::initializer_list<T>& args)
+{
+    first = std::make_shared<Node<T>>(Node<T>(*args.begin()));
+    auto current = first;
+
+    for(auto it = args.begin() + 1; it != args.end(); ++it)
+    {
+        current -> next = std::make_shared<Node<T>>(Node<T>(*it));
+        current = current -> next;
+    }
+}
+template<class T>
+void List<T>::push_front(const T _value)
+{
+    auto newNode = std::make_shared<Node<T>>(Node<T>(_value));
+
+    if(first != nullptr)
+    {
+        newNode -> next = first;
+    }
+
+    first = newNode;
+}
+
+template<class T>
+void List<T>::push_back(const T _value)
+{
+    auto newNode = std::make_shared<Node<T>>(Node<T>(_value));
+
+    if(first == nullptr)
+    {
+        first = newNode;
+        return;
+    }
+
+    std::shared_ptr<Node<T>> current = first;
+
+    while(current -> next != nullptr)
+    {
+        current = current -> next;
+    }
+
+    current -> next = newNode;
+}
+
+template<class T>
+void List<T>::pop_front()
+{
+    if(first == nullptr)
+    {
+        return;
+    }
+
+    if(first -> next == nullptr)
+    {
+        first == nullptr;
+        return;
+    }
+
+    std::shared_ptr<Node<T>> second = first -> next;
+    first = second;
+}
+
+template<class T>
+void List<T>::pop_back()
+{
+    if(first == nullptr)
+    {
+        return;
+    }
+
+    if(first -> next == nullptr)
+    {
+        first == nullptr;
+        return;
+    }
+
+    std::shared_ptr<Node<T>> current = first;
+
+    while(current -> next -> next != nullptr)
+    {
+        current = current -> next;
+    }
+
+    current -> next = nullptr;
+}
+
+template<class T>
+void List<T>::clear()
+{
+    if(!empty())
     {
         first = nullptr;
     }
+}
 
-    List(const std::initializer_list<T>& args)
+template<class T>
+void List<T>::print()
+{
+    std::shared_ptr<Node<T>> current = first;
+
+    while(current != nullptr)
     {
-        first = std::make_shared<Node<T>>(Node<T>(*args.begin()));
-        auto current = first;
+        std::cout << current -> value << " ";
+        current = current -> next;
+    }
+}
 
-        for(auto it = args.begin() + 1; it != args.end(); ++it)
-        {
-            current -> next = std::make_shared<Node<T>>(Node<T>(*it));
-            current = current -> next;
-        }
+template<class T>
+bool List<T>::empty()
+{
+    return first == nullptr;
+}
+
+template<class T>
+size_t List<T>::size()
+{
+    size_t counter = 0;
+
+    std::shared_ptr<Node<T>> current = first;
+
+    while(current != nullptr)
+    {
+        ++counter;
+        current = current -> next;
     }
 
-    void push_front(T _value)
+    return counter;
+}
+
+template<class T>
+T List<T>::at(const size_t pos)
+{
+    if(pos >= size())
     {
-	    auto newNode = std::make_shared<Node<T>>(Node<T>(_value));
-
-  	    if(first != nullptr)
-        {
-  		    newNode -> next = first;
-        }
-
-  	    first = newNode;
+        throw std::out_of_range("Index out of range");
     }
 
-    void push_back(T _value)
+    size_t index = 0;
+    std::shared_ptr<Node<T>> current = first;
+
+    while(index < pos)
     {
-        auto newNode = std::make_shared<Node<T>>(Node<T>(_value));
-
-        if(first == nullptr)
-        {
-            first = newNode;
-            return;
-        }
-
-        std::shared_ptr<Node<T>> current = first;
-
-        while(current -> next != nullptr)
-        {
-            current = current -> next;
-        }
-
-        current -> next = newNode;
+        current = current -> next;
+        ++index;
     }
 
-    void pop_front()
-    {
-        if(first == nullptr)
-        {
-            return;
-        }
+    return current -> value;
+}
 
-        if(first -> next == nullptr)
-        {
-            first == nullptr;
-            return;
-        }
-
-        std::shared_ptr<Node<T>> second = first -> next;
-        first = second;
-    }
-
-    void pop_back()
-    {
-        if(first == nullptr)
-        {
-            return;
-        }
-
-        if(first -> next == nullptr)
-        {
-            first == nullptr;
-            return;
-        }
-
-        std::shared_ptr<Node<T>> current = first;
-
-        while(current -> next -> next != nullptr)
-        {
-            current = current -> next;
-        }
-
-        current -> next = nullptr;
-    }
-
-    void print()
+template<class T>
+T List<T>::min()
+{
+    if(!empty())
     {
         std::shared_ptr<Node<T>> current = first;
+        T minimum = current -> value;
 
         while(current != nullptr)
         {
-            std::cout << current -> value << " ";
+            if(current -> value < minimum)
+            {
+                minimum = current -> value;
+            }
+
             current = current -> next;
         }
+
+        return minimum;
     }
+    return 0;
+}
 
-    size_t size()
+template<class T>
+T List<T>::max()
+{
+    if(!empty())
     {
-        size_t counter = 0;
-
         std::shared_ptr<Node<T>> current = first;
+        T maximum = current -> value;
 
         while(current != nullptr)
         {
-            ++counter;
+            if(current -> value > maximum)
+            {
+                maximum = current -> value;
+            }
+
             current = current -> next;
         }
 
-        return counter;
+        return maximum;
     }
-
-    bool empty()
-    {
-        return first == nullptr;
-    }
-
-    T min()
-    {
-        if(!empty())
-        {
-            std::shared_ptr<Node<T>> current = first;
-            T minimum = current -> value;
-
-            while(current != nullptr)
-            {
-                if(current -> value < minimum)
-                {
-                    minimum = current -> value;
-                }
-
-                current = current -> next;
-            }
-
-            return minimum;
-        }
-        return 0;
-    }
-
-    T max()
-    {
-        if(!empty())
-        {
-            std::shared_ptr<Node<T>> current = first;
-            T maximum = current -> value;
-
-            while(current != nullptr)
-            {
-                if(current -> value > maximum)
-                {
-                    maximum = current -> value;
-                }
-
-                current = current -> next;
-            }
-
-            return maximum;
-        }
-        return 0;
-    }
-
-    void clear()
-    {
-        if(!empty())
-        {
-            first = nullptr;
-        }
-    }
-
-    T at(const size_t pos)
-    {
-        if(pos >= size())
-        {
-            throw std::out_of_range("Index out of range");
-        }
-
-        size_t index = 0;
-        std::shared_ptr<Node<T>> current = first;
-
-        while(index < pos)
-        {
-            current = current -> next;
-            ++index;
-        }
-
-        return current -> value;
-    }
-};
+    return 0;
+}
